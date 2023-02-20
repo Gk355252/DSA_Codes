0b8f7431c6f63390939429c813c61ca5c733cabc
Gk355252 <103330862+Gk355252@users.noreply.github.com>
2023-02-20 10:48:26 +0530

    initial commit

---
+++ recursion_27.cpp
@@ -0,0 +1,21 @@
+//return subsequences of string using exclude and include method
+#include<iostream>
+using namespace std;
+void print_subs(string input, string output){
+    //base case
+    if (input.length()==0){
+        cout<<output<<endl;
+        return;
+    }
+    print_subs(input.substr(1),output);
+    print_subs(input.substr(1),output + input[0]);
+}
+int main()
+{
+string input;
+cin>>input;
+string output = "";
+print_subs(input,output);
+
+    return 0;
+}
